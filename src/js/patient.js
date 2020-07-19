41d0b6cdee44317d2a7b28b2375c271fe2c5382c
ABDUL MANNAN MALIK <malikmanan97@gmail.com>
2020-07-20 00:48:39 +0530

    API integration

--- src/js/patient.js
+++ src/js/patient.js
@@ -1,4 +1,9 @@
+const URL = "https://sehat.hyderdevelops.ml";
+
 const search = document.querySelector("#search");
+
+const errorResult = document.querySelector(".result_error");
+const create = document.querySelector("#create");
 const resultName = document.querySelector("#result_name");
 const apply = document.querySelector("#apply");
 const age = document.querySelector(".display_age");
@@ -8,27 +13,111 @@ const residence = document.querySelector(".display_residence");
 const user = document.querySelector(".display_name");
 const phoneIp = document.querySelector("#phone-input");
 const next = document.querySelector("#next");
+const inputContainer = document.querySelector(".input_details");
+const displayContainer = document.querySelector(".display_details");
+const label = "object";
 
-next.addEventListener("click", () => {
+let data = {};
+
+next.addEventListener("click", async () => {
   window.location = "../public/test.html";
 });
 
-search.addEventListener("click", () => {
+const options = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+search.addEventListener("click", async () => {
   if (phoneIp.value.trim() === "") {
     phoneIp.style.border = "2px solid red";
     return false;
-  } else {
-    setTimeout(() => {
-      resultName.textContent = "Abdul Mannan Malik";
-    }, 1000);
+  }
+  try {
+    const response = await fetch(
+      `${URL}/users/getUserByPhone?phone=${phoneIp.value}`,
+      options
+    );
+    const text = await response.text();
+    data = JSON.parse(text);
+    result.textContent = data.FULL_NAME;
+  } catch (err) {
+    resultName.style.display = "none";
+    create.style.display = "block";
+    errorResult.style.display = "block";
+    apply.style.display = "none";
+    err.style.transition = "all .3s";
   }
 });
 apply.addEventListener("click", () => {
-  setTimeout(() => {
-    user.textContent = "Abdul Manan Malik";
-    age.textContent = "22";
-    phone.textContent = "7006982627";
-    email.textContent = "malikmanan97@gmail.com";
-    residence.textContent = "341, Jawahar-Nagar";
-  }, 1000);
+  user.textContent = data.FULL_NAME;
+  age.textContent = data.AGE;
+  phone.textContent = data.PHONE;
+  email.textContent = data.EMAIL;
+  residence.textContent = data.REGION;
+});
+// 9652145897
+
+const dateField = document.querySelector("#patient-age");
+const userInput = document.querySelector(".form_input");
+
+dateField.addEventListener("keyup", (e) => {
+  if (e.which !== 8) {
+    let numChars = e.target.value.length;
+    if (numChars === 2 || numChars === 5) {
+      let thisVal = e.target.value;
+      thisVal += "/";
+      e.target.value = thisVal;
+    }
+  }
+});
+
+create.addEventListener("click", async () => {
+  inputContainer.style.display = "block";
+
+  displayContainer.style.display = "none";
+
+  const FULL_NAME = document.querySelector("#patient-name").value;
+  const EMAIL = document.querySelector("#patient-email").value;
+  const REGION = document.querySelector("#patient-address").value;
+  const HEIGHT = document.querySelector("#patient-height").value;
+  const WEIGHT = document.querySelector("#patient-weight").value;
+  const KNOWN_ILLNESS = [`Diabetes`];
+  const PHONE = document.querySelector("#patient-phone").value;
+
+  var dateformat = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+
+  // Age Calculation
+  const userDate = new Date(dateField.value).getFullYear();
+  const currDate = new Date().getFullYear();
+  const AGE = currDate - userDate;
+
+  if (!dateformat.test(dateField.value)) {
+    dateField.style.borderBottom = `3px solid red`;
+    return false;
+  }
+  // Fetch
+  const data = {
+    FULL_NAME,
+    EMAIL,
+    REGION,
+    AGE,
+    HEIGHT,
+    WEIGHT,
+    KNOWN_ILLNESS,
+    PHONE,
+  };
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  const response = await fetch(`${url}/users/register`, options);
+  const status = response.status;
+  console.log(status);
 });
